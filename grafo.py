d74d8d4d502fb2467ed8d7ac00c0402d45d312a6
vsosmonteiro <47899434+vsosmonteiro@users.noreply.github.com>
2020-12-01 17:21:55 -0300

    Update grafo.py

--- grafo.py
+++ grafo.py
@@ -1,5 +1,8 @@
 import random
 import pandas
+import copy
+arquivo=pandas.read_csv("Tweets.csv")
+
 class Adj:
     def __init__(self,v,peso):
         self.vertice=v
@@ -7,10 +10,25 @@ class Adj:
         self.prox=None
 
 class Vertice:
-    def __init__(self):
+    def __init__(self,arquivo,x,i):
+
         self.cabeca = None
         self.nvizinhos=0
-        self.dado=None
+        self.vertice=i
+        self.tweetId=arquivo['tweet_id'][x]
+        self.airline_sentiment=arquivo['airline_sentiment'][x]
+        self.airline_sentiment_confidence=arquivo['airline_sentiment_confidence'][x]
+        self.negativereason=arquivo['negativereason'][x]
+        self.negativereason_confidence=arquivo['negativereason_confidence'][x]
+        self.airline=arquivo['airline'][x]
+        self.airline_sentiment_gold=arquivo['airline_sentiment_gold'][x]
+        self.name=arquivo['name'][x]
+        self.negativereason_gold=arquivo['negativereason_gold'][x]
+        self.retweet_count=arquivo['retweet_count'][x]
+        self.tweet_coord=arquivo['tweet_coord'][x]
+        self.tweet_created=arquivo['tweet_created'][x]
+        self.tweet_location=arquivo['tweet_location'][x]
+        self.user_timezone=arquivo['user_timezone'][x]
 
 class initD:
     def __init__(self,v):
@@ -28,10 +46,12 @@ class Grafo:
         self.verticies=[]
 
 def criarvertice(grafo):
-    for i in range(100):
-        grafo.verticies.append(Vertice())
+    for i in range(grafo.nvert):
+        x=random.randint(0,len(arquivo))
+        grafo.verticies.append(Vertice(arquivo,x,i))
 
 def criararesta(vi,vf,grafo,peso):
+    fk=grafo.verticies
     novo=Adj(vf,peso)
     grafo.verticies[vi].nvizinhos+=1
     novo.prox=grafo.verticies[vi].cabeca
@@ -39,13 +59,15 @@ def criararesta(vi,vf,grafo,peso):
     grafo.arestas+=1
 def printargf(grafo):
 
-    for i in range (5):
+    for i in range (grafo.nvert):
         print("vertice=",i)
-        j=i
-        fk=grafo.verticies
-        for i in range(fk[j].nvizinhos):
-            print(fk[j].cabeca.vertice)
-            fk[j].cabeca=fk[j].cabeca.prox
+        fk=grafo.verticies[i]
+        fk2=grafo.verticies[i].cabeca
+        
+        print(fk.tweetId)
+       
+
+
 def relaxa(grafo,listas,u,v):
     ad=grafo.verticies[u].cabeca
     while (ad and ad.vertice!=v):
@@ -54,8 +76,20 @@ def relaxa(grafo,listas,u,v):
         if(listas.d[v] > listas.d[u]+ ad.peso):
             listas.d[v] = listas.d[u]+ad.peso
             listas.p[v] = u
+def caminho(grafo,inicial,final,listas):
+    newlist=[]
+    i=final
+    
+    while(i!=inicial):
+        newlist.append(i)
+        i=listas.p[i]
+        
+    newlist.append(inicial)    
+    newlist.reverse()
+    
+    print(newlist)
 
-def dijsktra(grafo,vertice):
+def dijsktra(grafo,vertice,final):
     aberto=[]
     listas=initD(vertice)
     for i in range(grafo.nvert):
@@ -67,8 +101,9 @@ def dijsktra(grafo,vertice):
         while(ad):
             relaxa(grafo,listas,u,ad.vertice)
             ad=ad.prox
-    for i in range(grafo.nvert):
-        print(listas.d[i])
+    print(listas.d[final])
+    caminho(grafo,vertice,final,listas)
+
 def isopen(grafo,aberto):
     for i in range(grafo.nvert):
         if(aberto[i]==1):
@@ -91,16 +126,31 @@ def smallerdist(grafo,aberto,listas):
 
     return menor
 
+def buscar(grafo,id):
+    for i in range(grafo.nvert):
+        if(grafo.verticies[i].tweetId==int(id)):
+            return i
 
-grafo=Grafo(6)
+grafo=Grafo(10)
 criarvertice(grafo)
-criararesta(0,1,grafo,10)
-criararesta(0,2,grafo,5)
-criararesta(2,1,grafo,3)
-criararesta(1,3,grafo,1)
-criararesta(2,3,grafo,8)
-criararesta(2,4,grafo,2)
-criararesta(4,5,grafo,6)
-criararesta(3,5,grafo,4)
-criararesta(3,4,grafo,4)
-dijsktra(grafo,0)
+criararesta(0,1,grafo,2)
+criararesta(1,4,grafo,2)
+criararesta(1,3,grafo,2)
+criararesta(2,3,grafo,2)
+criararesta(3,4,grafo,2)
+criararesta(4,5,grafo,2)
+criararesta(5,6,grafo,2)
+criararesta(6,7,grafo,2)
+criararesta(7,8,grafo,2)
+criararesta(8,2,grafo,2)
+criararesta(8,9,grafo,2)
+criararesta(9,8,grafo,2)
+deleditar=input("1=print,2=djiktra,0=sair: ")
+
+while(int(deleditar)!=0):
+    if(int(deleditar)==1):
+        printargf(grafo)
+    if(int(deleditar)==2):
+
+        dijsktra(grafo,buscar(grafo,input("coloque o id")),buscar(grafo,input("destino")))
+    deleditar=input("1=print,2=djiktra,0=sair: ")
